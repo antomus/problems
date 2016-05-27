86c927b043b3ae90a23ccdb7a4a1e68541f7fbbe
antomus <antomus81@gmail.com>
2016-05-27 10:40:58 +0300

    change sigkey

--- sigkey/sigkey.rb
+++ sigkey/sigkey.rb
@@ -1,21 +1,23 @@
 #!/usr/bin/env ruby
 class SigKeyProcessor
 
-  ALPHABET_ARRAY = [ 97, 98, 99, 100,  
+  ALPHABET_ARRAY = [ 97, 98, 99, 100,
                     101, 102, 103, 104,  
                     105, 106, 107, 108, 
                     109, 110, 111, 112, 
                     113, 114, 115, 116,  
-                    117, 118, 119, 120, 
+                    117, 118, 119, 120,
                     121, 122 ].freeze
 
   def initialize(input_file, output_file)
     @input_file = input_file || "sigkey.in"
     @output_file = output_file || "sigkey.out" 
     @total_pairs_count, @keys_array = read_input_data
-    @alphabet_sums = []
-    ALPHABET_ARRAY.each_with_index { |_,i| @alphabet_sums << ALPHABET_ARRAY[0..i].reduce(:+) }
-    @alphabet_sums.freeze
+    @alphabet_sums = {}
+    ALPHABET_ARRAY.each_with_index do |_,i| 
+      @alphabet_sums[ALPHABET_ARRAY[0..i].length] = ALPHABET_ARRAY[0..i].reduce(:+)
+    end
+    @alphabet_sums .freeze
   end
 
   def execute
@@ -28,19 +30,12 @@ class SigKeyProcessor
       #@keys_array.delete(key)
       @keys_array.each_with_index do |other_key,j|
         next if @keys_array[i].nil? || @keys_array[j].nil?
-        sum_str = (key + other_key)
-        sum_str.each_bytes.reduce(&:+)
-        next if !sum_str.include?("a")
-        min = sum_str.min
-        max = sum_str.max
-        sum_str_length = sum_str.length
-        #sequence_length = (sum_str[0]..sum_str[sum_str_length - 1]).to_a.length
-        sequence_length = max.ord - min.ord + 1
+
 
         #puts "sum_str_length #{sum_str_length}"
         #puts "sequence_length #{sequence_length}"
 
-        if sequence_length == sum_str_length
+        if key_pair?(key, other_key)
           counter += 1
           @keys_array[i] = nil
           @keys_array[j] = nil
@@ -52,8 +47,10 @@ class SigKeyProcessor
 
   private
 
-  def key_pair?(key)
-    
+  def key_pair?(key, other_key)
+    sum_str = key + other_key
+    pair_sum = sum_str.each_byte.reduce(&:+)
+    @alphabet_sums[sum_str.length] && @alphabet_sums[sum_str.length] == pair_sum
   end
 
   def read_input_data
